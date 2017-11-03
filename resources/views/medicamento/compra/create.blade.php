9a8b1fc70e6f3eb77f9770501c060c3fcc8fadd4
ldrl <drdanielreyes5@gmail.com>
2017-11-03 12:36:33 -0600

    correciones

--- resources/views/medicamento/compra/create.blade.php
+++ resources/views/medicamento/compra/create.blade.php
@@ -73,6 +73,60 @@
                 autoclose: true
                 
         });
+
+
+
+    $(document).on('click','.btn-btnGuardarCompra',function(e){
+        var urlraiz=$("#url_raiz_proyecto").val();
+        var miurl=urlraiz+"/medicamento/compra/store";
+        $.ajaxSetup({
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="_token"]').attr('content')
+            }
+        });
+
+        var formData = {
+            proveedor: $("#idproveedor").val(),
+            medicamento: $("#idmedicamento").val(),
+            fecha_compra: $("#buy_date").val(),
+            fecha_vencimiento: $("#expiration_date").val(),
+            precio: $("#precio").val(),
+            cantidad: $("#cantidad").val(),
+            ubicacion: $("#idubicacion").val(),
+        };
+
+        $.ajax({
+            type: "POST",
+            url: miurl,
+            data: formData,
+            dataType: 'json',
+            success: function (data) {
+                $('#formAgregarCompra').trigger("reset");
+                $('#formModalUsuario').modal('hide');
+                swal({
+                    title:"Envio correcto",
+                    text: "Gracias",
+                    type: "success"
+                }).then(function () {
+                    cargarindex(5);
+                });
+            },
+            error: function (data) {
+                $('#loading').modal('hide');
+                var errHTML="";
+                if((typeof data.responseJSON != 'undefined')){
+                    for( var er in data.responseJSON.errors){
+                        errHTML+="<li>"+data.responseJSON.errors[er]+"</li>";
+                    }
+                }else{
+                    errHTML+='<li>Error</li>';
+                }
+
+                $("#erroresContentMedicamento").html(errHTML); 
+                $('#erroresModalMedicamento').modal('show');
+            }
+        });
+    });
 </script>
 
 
